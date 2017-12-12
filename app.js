4498d4d9128b596af391371d4b5e7e0eec4efbf6
brianmituka <mitukab@outlook.com>
2017-12-12 14:37:23 +0300

    addes hex generation

---
+++ app.js
@@ -0,0 +1,16 @@
+function getColor() {
+  //generating a random hexadecimal number and slice it to remove . and get 6 characters
+  return '#' + Math.random().toString(16).slice(2,8);
+}
+//sets background color style
+function setBackground(){
+  var background = getColor();
+  document.body.style.background = background;
+}
+// runs function on click
+document.body.onkeyup = function(e){
+  if(e.keyCode == 32){
+    setBackground();
+    console.log(getColor());
+  }
+}
